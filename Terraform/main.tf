96ebdfb7b784e2ca42377ce69cf90e8e4754ba65
Leoalmeida96 <leo.almeida.silva@hotmail.com>
2021-07-29 11:38:13 -0300

    configurando security group e ssh

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,4 +11,22 @@ resource "aws_instance" "dev" {
     tags ={
         Name = "dev${count.index}"
     }
+    vpc_security_group_ids = ["sg-0494f94ad5a9858c"]
+}
+
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["191.45.92.202/32"]
+  }
+
+    tags ={
+        Name = "ssh"
+    }
 }
