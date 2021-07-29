319dd18922e69cd714f6d0d6d883154f86c793e4
Leoalmeida96 <leo.almeida.silva@hotmail.com>
2021-07-29 14:18:39 -0300

    criando maquinas com dependencia para bucket

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,7 +11,28 @@ resource "aws_instance" "dev" {
     tags ={
         Name = "dev${count.index}"
     }
-    vpc_security_group_ids = ["sg-0494f94ad5a9858c"]
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_instance" "dev4" {
+    ami = "ami-026c8acd92718196b"
+    instance_type = "t2.mico"
+    key_name = "terraform-aws"
+    tags ={
+        Name = "dev4"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+    depends_on = [aws_s3_bucket.dev4]
+}
+
+resource "aws_instance" "dev5" {
+    ami = "ami-026c8acd92718196b"
+    instance_type = "t2.mico"
+    key_name = "terraform-aws"
+    tags ={
+        Name = "dev5"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
 
 resource "aws_security_group" "acesso-ssh" {
@@ -29,4 +50,13 @@ resource "aws_security_group" "acesso-ssh" {
     tags ={
         Name = "ssh"
     }
+}
+
+resource "aws_s3_bucket" "dev4" {
+  bucket = "lalmlab-dev4"
+  acl    = "private"
+
+  tags = {
+    Name = "lalmlab-dev4"
+  }
 }
